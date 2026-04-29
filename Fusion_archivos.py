02269ad2025edadff6e79a2642894990b6232680
marcelportaz <marcel.portaz@alum.upf.edu>
2026-04-29 18:52:31 +0200

    Eliminar duplicados

--- Fusion_archivos.py
+++ Fusion_archivos.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
+
+
 df_features_audio = pd.read_csv('./mis_features_audio.csv')
 df_asvspoof = pd.read_csv('./Audios/DS_LA/ASVspoof_LA.csv')
 # Realizar el merge usando la columna 'file_name' de df_features_audio y 'File_Name' de df_asvspoof
 df_fusionado = pd.merge(df_features_audio, df_asvspoof, left_on='file_name', right_on='File_Name', how='inner')
+# Eliminamos  la columna 'File_Name' para no contar con duplicados
+df_fusionado.drop(columns=['File_Name'], inplace=True)
 # Guardar el DataFrame fusionado en un nuevo CSV
 df_fusionado.to_csv('./mis_features_fusionadas.csv', index=False)
 print("¡Fusión completada! El archivo 'mis_features_fusionadas.csv' ha sido creado con éxito.")
